d6abdee506bf52ae7c23d943ef5df05f9415bb3a
Alive <867912954@qq.com>
2020-11-29 16:03:38 +0800

    second commit

--- 20201129.py
+++ 20201129.py
@@ -4,8 +4,36 @@
 """
     有四个数字：1,2,3,4，能组成多个个互不相同且不重复的三位数？各是多少？
 """
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if(i!=k) and (j!=k) and (i!=j):
-                print(i,j,k)
+
+for i in range(1, 5):  # 百位
+    for j in range(1, 5):  # 十位
+        for k in range(1, 5):  # 个位
+            if (i != k) and (j != k) and (i != j):
+                print(i, j, k)
+# result
+"""
+1 2 3
+1 2 4
+1 3 2
+1 3 4
+1 4 2
+1 4 3
+2 1 3
+2 1 4
+2 3 1
+2 3 4
+2 4 1
+2 4 3
+3 1 2
+3 1 4
+3 2 1
+3 2 4
+3 4 1
+3 4 2
+4 1 2
+4 1 3
+4 2 1
+4 2 3
+4 3 1
+4 3 2
+"""
